f7e4935ce61fc44b808be190cc6543b933077fc0
apezzatto <rogerio.meneghelo@gmail.com>
2018-03-02 15:48:34 -0500

    Added third party components

--- DatingApp/DatingApp.SPA/src/app/app.component.ts
+++ DatingApp/DatingApp.SPA/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './_services/auth.service';
+import { JwtHelper } from 'angular2-jwt';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'datingn app';
+  jwtHelper: JwtHelper = new JwtHelper();
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.authService.decodedToken = this.jwtHelper.decodeToken(token);
+    }
+  }
 }
